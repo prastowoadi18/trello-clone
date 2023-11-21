e4dbb85da6fd1e631b1fd32e65c044c6cbdfaf18
PrastowoAdi <sinaadi75@gmail.com>
2023-11-22 06:53:25 +0700

    add feature add title board

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,17 @@
-const OrganizationIdPage = () => {
-  return <div>OrganizationIdPage</div>;
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_component/info";
+import { BoardList } from "./_component/board-list";
+
+const OrganizationIdPage = async () => {
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
