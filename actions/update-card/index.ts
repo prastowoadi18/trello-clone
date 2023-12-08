28b68ee28cca629fe4926c7a50cee51b8c6790e6
PrastowoAdi <sinaadi75@gmail.com>
2023-12-08 10:56:51 +0700

    add modal, react query

---
+++ actions/update-card/index.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { InputType, ReturnType } from "./type";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { UpdateCard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { orgId, userId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unathorized",
+    };
+  }
+
+  const { id, boardId, ...values } = data;
+  let card;
+
+  try {
+    card = await db.card.update({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      data: {
+        ...values,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to update.",
+    };
+  }
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const updateCard = createSafeAction(UpdateCard, handler);
