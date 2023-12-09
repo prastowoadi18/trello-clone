a100e704946205c72a390b26095491df477f16a4
PrastowoAdi <sinaadi75@gmail.com>
2023-12-09 17:46:56 +0700

    add limit create board

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,6 +8,7 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateBoard } from "./schema";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { hasAvailableCount, incrementAvailableCount } from "@/lib/org-limit";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -15,6 +16,15 @@ const handler = async (data: InputType): Promise<ReturnType> => {
   if (!userId || !orgId) {
     return {
       error: "Unauthorized",
+    };
+  }
+
+  const canCreate = await hasAvailableCount();
+
+  if (!canCreate) {
+    return {
+      error:
+        "You have reached your limit of free boards. Please upgrade to create more.",
     };
   }
 
@@ -48,6 +58,8 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageThumbUrl,
       },
     });
+
+    await incrementAvailableCount();
 
     await createAuditLog({
       entityTitle: board.title,
--- app/(platform)/(dashboard)/organization/[organizationId]/_component/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_component/board-list.tsx
@@ -1,7 +1,9 @@
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
 import { Skeleton } from "@/components/ui/skeleton";
+import { MAX_FREE_BOARDS } from "@/constants/board";
 import { db } from "@/lib/db";
+import { getAvailableCount } from "@/lib/org-limit";
 import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2 } from "lucide-react";
 import Link from "next/link";
@@ -23,13 +25,15 @@ export const BoardList = async () => {
     },
   });
 
+  const availableCount = await getAvailableCount();
+
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
         <User2 className="h-6 w-6 mr-2" />
         Your boards
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
         {boards.map((board) => (
           <Link
             key={board.id}
@@ -49,7 +53,9 @@ export const BoardList = async () => {
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${
+              MAX_FREE_BOARDS - availableCount
+            } remaining`}</span>
             <Hint
               sideOffset={40}
               description={`Free Workspaces can have to 5 open boards. For unlimited boars upgrade this workspace.`}
